a2a50591a5f581f76ce90b69071b4e01c60d5624
arubinofaux <arubinofaux@gmail.com>
2019-10-07 19:24:23 -0400

    wip

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,5 @@
 class Match < ApplicationRecord
   belongs_to :player, class_name: "Player", foreign_key: :player_id
   belongs_to :opponent, class_name: "Player", foreign_key: :opponent_id
+  belongs_to :winner, class_name: "Player", foreign_key: :winner
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -2,6 +2,7 @@ class Player < ApplicationRecord
   belongs_to :user, optional: true
   has_many :player_matches, class_name: "Match", foreign_key: :player_id
   has_many :opponent_matches, class_name: "Match", foreign_key: :opponent_id
+  has_many :winner_matches, class_name: "Match", foreign_key: :winner
 
   validates :bnet_name, presence: true
 end
